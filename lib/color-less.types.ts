1f4c3fc3fa8f0e43dc0aea818b8acede1aea058a
cipchk <cipchk@qq.com>
2020-08-15 16:25:18 +0800

    update

--- lib/color-less.types.ts
+++ lib/color-less.types.ts
@@ -1,6 +1,6 @@
 export interface ColorLessConfig {
   /**
-   * Specify the name of the theme variables to be changed, default is `@primary-color`
+   * Specify the name of the theme variables to be changed, default is `[ '@primary-color' ]`
    * Can be set all antd & ng-alain custom theme variables
    */
   variables?: string[];
